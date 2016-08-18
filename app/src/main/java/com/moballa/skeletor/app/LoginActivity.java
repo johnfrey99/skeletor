dca0abe27575dfff9f37e622bbae0f2c09d1d3aa
John Frey <johnfrey99@gmail.com>
2016-08-18 18:45:42 -0500

    refactor for lambdas and butterknife

--- app/src/main/java/com/moballa/skeletor/app/LoginActivity.java
+++ app/src/main/java/com/moballa/skeletor/app/LoginActivity.java
@@ -3,31 +3,28 @@ package com.moballa.skeletor.app;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -56,38 +53,29 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 	private UserLoginTask mAuthTask = null;
 
 	// UI references.
-	private AutoCompleteTextView mEmailView;
-	private EditText mPasswordView;
-	private View mProgressView;
-	private View mLoginFormView;
+	@BindView(R.id.email) AutoCompleteTextView mEmailView;
+	@BindView(R.id.password) EditText mPasswordView;
+	@BindView(R.id.login_progress) View mProgressView;
+	@BindView(R.id.email_sign_in_button) Button mEmailSignInButton;
+	@BindView(R.id.email_login_form) View mLoginFormView;
 
 	@Override protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_activity);
+		ButterKnife.bind(this);
+
 		// Set up the login form.
-		mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
 		populateAutoComplete();
 
-		mPasswordView = (EditText) findViewById(R.id.password);
-		mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-			@Override public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-				if (id == R.id.login || id == EditorInfo.IME_NULL) {
-					attemptLogin();
-					return true;
-				}
-				return false;
-			}
-		});
-
-		Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-		mEmailSignInButton.setOnClickListener(new OnClickListener() {
-			@Override public void onClick(View view) {
+		mPasswordView.setOnEditorActionListener((textView, id, keyEvent) -> {
+			if (id == R.id.login || id == EditorInfo.IME_NULL) {
 				attemptLogin();
+				return true;
 			}
+			return false;
 		});
 
-		mLoginFormView = findViewById(R.id.login_form);
-		mProgressView = findViewById(R.id.login_progress);
+		mEmailSignInButton.setOnClickListener(view -> attemptLogin());
 	}
 
 	private void populateAutoComplete() {
@@ -107,11 +95,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 		}
 		if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
 			Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-					.setAction(android.R.string.ok, new View.OnClickListener() {
-						@Override @TargetApi(Build.VERSION_CODES.M) public void onClick(View v) {
-							requestPermissions(new String[] { READ_CONTACTS }, REQUEST_READ_CONTACTS);
-						}
-					});
+					.setAction(android.R.string.ok,
+							v -> requestPermissions(new String[] { READ_CONTACTS }, REQUEST_READ_CONTACTS));
 		} else {
 			requestPermissions(new String[] { READ_CONTACTS }, REQUEST_READ_CONTACTS);
 		}
